ee81561a0b6d5d62424461eaf0a2781f1ace4145
BrianEdmond <brianwedmond@gmail.com>
2015-10-06 21:14:48 -0400

    Login completed.

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,8 +3,20 @@ blocitoff = angular.module('blocitoff', ['ui.router', 'firebase', 'ui.sortable']
 blocitoff.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 
-  $stateProvider.state('home', {
+  $stateProvider.state('register', {
     url: '/',
+    controller: 'Register.controller',
+    templateUrl: '/templates/register.html'
+  });
+
+  $stateProvider.state('login', {
+    url: '/login',
+    controller: 'Login.controller',
+    templateUrl: '/templates/login.html'
+  });
+
+  $stateProvider.state('home', {
+    url: '/home',
     controller: 'Home.controller',
     templateUrl: '/templates/home.html'
   });
@@ -41,7 +53,7 @@ blocitoff.controller('Home.controller', ['$scope', '$firebaseArray', 'FIREBASE_U
 
   };
 
-    //function to sort 
+  //function to sort 
   $scope.sortableOptions = {
     update: function(event, ui) {
       $scope.tasks.forEach(function(task) {
@@ -65,7 +77,7 @@ blocitoff.controller('Home.controller', ['$scope', '$firebaseArray', 'FIREBASE_U
     var timeNow = task.created_at;
 
     //change to 604800000 after testing
-    if(task.condition == "active" && (timestamp - timeNow) >= 600000) {
+    if(task.condition == "active" && (timestamp - timeNow) >= 604800000) {
       task.condition = "complete";
       $scope.tasks.$save(task);
     }
@@ -80,4 +92,49 @@ blocitoff.controller('Complete.controller', ['$scope', '$firebaseArray', 'FIREBA
   $scope.tasks = $firebaseArray(ref);
 
 }]);
+
+//register controller
+blocitoff.controller('Register.controller', ['$scope', '$firebaseArray', 'FIREBASE_URL', function($scope, $firebaseArray, FIREBASE_URL) {
+
+  var ref = new Firebase(FIREBASE_URL);
+
+  //register account
+  $scope.register = function() {
+    ref.createUser({
+      email: $scope.userData,
+      password: $scope.authData
+    }, function(error, userData) {
+      if (error) {
+        console.log("Error creating user:", error);
+      } else {
+        console.log("User account created with uid:", userData.uid);
+      }
+    });
+  }
+
+}]);
+
+//login controller
+blocitoff.controller('Login.controller', ['$scope', '$firebaseArray', 'FIREBASE_URL', function($scope, $firebaseArray, FIREBASE_URL) {
+
+  var ref = new Firebase(FIREBASE_URL);
+  
+  //account login
+  $scope.login = function() {
+    ref.authWithPassword({
+      email: $scope.userData,
+      password: $scope.authData
+    }, function(error, authData) {
+      if (error) {
+        console.log("Login failed.", error);
+      } else {
+        $scope.loggedIn = true;
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
+  }
+
+}]);
+
+
 
